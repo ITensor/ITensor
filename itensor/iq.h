ITensor/ITensor

commit 066d8e00683714bc14f7508def003291fdeaa68a

Added method IQTensor::norm()

itensor/iq.h
@@ -754,6 +754,14 @@ public:
         if(I.type() == t) ++count;
         return count;
     }
+
+    Real norm() const
+    {
+        Real res;
+        foreach(const ITensor& t, itensor) res += sqrt(t.norm());
+        return sqrt(res);
+    }
+
     int vec_size() const
     {
         int s = 0;

commit 262d2908fd3c180968388ee3049a97e2b3f1b5af

Fixed incorrect definition of IQTensor::norm() (introduced in 066d8e0).

itensor/iq.h
@@ -971,7 +971,7 @@ public:
     Real norm() const
     {
         Real res;
-        foreach(const ITensor& t, p->itensor) res += sqrt(t.norm());
+        foreach(const ITensor& t, p->itensor) res += sqr(t.norm());
         return sqrt(res);
     }
 