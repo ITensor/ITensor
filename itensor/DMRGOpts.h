83df52eeef61ec61016729e12cf6f5a0e17675f6
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2011-10-11 09:16:40 -0700

    Split declarations and header code for DMRGOpts.

--- itensor/DMRGOpts.h
+++ itensor/DMRGOpts.h
@@ -5,12 +5,6 @@
 //Class for fine-tuning DMRG algorithms
 class DMRGOpts : public BaseDMRGOpts
 {
-private:
-    Vector center_eigs;
-    Real energy_errgoal;    //Stop DMRG once energy has converged to this precision
-    Real orth_weight;       //How much to penalize non-orthogonality in multiple-state DMRG
-    bool printeigs;         //Print slowest decaying eigenvalues after every sweep
-    bool quiet_;             //Show/don't show info after every step
 public:
 
     bool quiet() const { return quiet_; }
@@ -25,53 +19,73 @@ public:
     bool printEigs() const { return printeigs; }
     void printEigs(bool val) { printeigs = val; }
     
-    DMRGOpts() : 
-    energy_errgoal(-1), 
-    orth_weight(1),
-    printeigs(true), 
-    quiet_(true)
-    { }
+    DMRGOpts();
 
     virtual ~DMRGOpts() { }
     
-    virtual void measure(int sw, int ha, int b, const SVDWorker& svd, Real energy)
-    {
-        if(printeigs)
-        {
-            if(b == 1 && ha == 2) 
-            {
-                std::cout << "\n    Largest m during sweep " << sw << " was " << svd.maxEigsKept() << "\n";
-                std::cout << "    Largest truncation error: " << svd.maxTruncerr() << "\n";
-                Vector center_eigs = svd.eigsKept(svd.NN()/2);
-                std::cout << "    Eigs at center bond: ";
-                for(int j = 1; j <= min(center_eigs.Length(),10); ++j) 
-                {
-                    std::cout << boost::format(center_eigs(j) > 1E-2 ? ("%.2f") : ("%.2E")) % center_eigs(j);
-                    std::cout << ((j != min(center_eigs.Length(),10)) ? ", " : "\n");
-                }
-                std::cout << boost::format("    Energy after sweep %d is %f\n") % sw % energy;
-            }
-        }
-    }
+    virtual void measure(int sw, int ha, int b, const SVDWorker& svd, Real energy);
     
-    virtual bool checkDone(int sw, Real energy)
+    virtual bool checkDone(int sw, Real energy);
+
+private:
+
+    Vector center_eigs;
+    Real energy_errgoal; //Stop DMRG once energy has converged to this precision
+    Real orth_weight;    //How much to penalize non-orthogonality in multiple-state DMRG
+    bool printeigs;      //Print slowest decaying eigenvalues after every sweep
+    bool quiet_;         //Show/don't show info after every step
+
+}; // class DMRGOpts
+
+inline DMRGOpts::
+DMRGOpts() 
+    : energy_errgoal(-1), 
+      orth_weight(1),
+      printeigs(true), 
+      quiet_(true)
+    { }
+
+inline
+void DMRGOpts::
+measure(int sw, int ha, int b, const SVDWorker& svd, Real energy)
     {
-        static Real last_energy;
-        
-        if(sw == 1) last_energy = 1000;
-        if(energy_errgoal > 0 && sw%2 == 0)
+    if(printeigs)
         {
-            Real dE = fabs(energy-last_energy);
-            if(dE < energy_errgoal)
+        if(b == 1 && ha == 2) 
             {
-                std::cout << boost::format("    Energy error goal met (dE = %E); returning after %d sweeps.\n") % dE % sw;
-                return true;
+            std::cout << "\n    Largest m during sweep " << sw << " was " << svd.maxEigsKept() << "\n";
+            std::cout << "    Largest truncation error: " << svd.maxTruncerr() << "\n";
+            Vector center_eigs = svd.eigsKept(svd.NN()/2);
+            std::cout << "    Eigs at center bond: ";
+            for(int j = 1; j <= min(center_eigs.Length(),10); ++j) 
+                {
+                std::cout << boost::format(center_eigs(j) > 1E-2 ? ("%.2f") : ("%.2E")) % center_eigs(j);
+                std::cout << ((j != min(center_eigs.Length(),10)) ? ", " : "\n");
+                }
+            std::cout << boost::format("    Energy after sweep %d is %f\n") % sw % energy;
             }
         }
-        last_energy = energy;
-        
-        return false;
     }
-}; // class DMRGOpts
+
+inline
+bool DMRGOpts::
+checkDone(int sw, Real energy)
+    {
+    static Real last_energy;
+    
+    if(sw == 1) last_energy = 1000;
+    if(energy_errgoal > 0 && sw%2 == 0)
+        {
+        Real dE = fabs(energy-last_energy);
+        if(dE < energy_errgoal)
+            {
+            std::cout << boost::format("    Energy error goal met (dE = %E); returning after %d sweeps.\n") % dE % sw;
+            return true;
+            }
+        }
+    last_energy = energy;
+    
+    return false;
+    }
 
 #endif // __ITENSOR_DMRGOPTS_H
