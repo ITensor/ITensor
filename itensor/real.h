dadc90809eebee56267ed29e848055b54dfcbc6c
Miles Stoudenmire (ja) <miles.stoudenmire@gmail.com>
2011-10-24 11:29:06 -0700

    New read/write for LogNumber; old one seemed to be causing machine dependent issues.

--- itensor/real.h
+++ itensor/real.h
@@ -39,100 +39,129 @@ class LogNumber
     int sign_;
 public:
 
-    Real logNum() const { return lognum_; }
-    int sign() const { return sign_; }
-    inline bool isRealZero() const
+    Real 
+    logNum() const { return lognum_; }
+
+    int 
+    sign() const { return sign_; }
+
+    inline bool 
+    isRealZero() const
         { return (sign_ == 0 || lognum_ < -maxlogdouble); }
-    inline bool isOne() const { return (lognum_ == 0 && sign_ == 1); }
-    inline bool isFiniteReal() const 
+
+    inline bool 
+    isOne() const { return (lognum_ == 0 && sign_ == 1); }
+
+    inline bool 
+    isFiniteReal() const 
         { return (lognum_ < maxlogdouble && lognum_ > -maxlogdouble); }
-    inline bool isNan() const 
+
+    inline bool 
+    isNan() const 
         { return lognum_ != lognum_; }
 
     //Default is Real(LogNum()) == 1
     LogNumber() : lognum_(0), sign_(1) { }
 
     LogNumber(Real r)
-	{
+        {
         if(r == 0)
             { sign_ = 0;  lognum_ = 0; }
         else if(r > 0)
             { sign_ = 1;  lognum_ = log(r); }
         else
             { sign_ = -1; lognum_ = log(-r); }
-	}
+        }
 
     LogNumber(Real lognum, int sign) : lognum_(lognum), sign_(sign) { } 
 
-    inline void read(std::istream& s) 
-        { s.read((char*)this,sizeof(this)); }
-    inline void write(std::ostream& s) const 
-        { s.write((char*)this,sizeof(this)); }
+    inline void 
+    read(std::istream& s) 
+        { 
+        s.read((char*) &lognum_,sizeof(lognum_)); 
+        s.read((char*) &sign_,sizeof(sign_)); 
+        }
+    inline void 
+    write(std::ostream& s) const 
+        { 
+        s.write((char*) &lognum_,sizeof(lognum_)); 
+        s.write((char*) &sign_,sizeof(sign_)); 
+        }
 
     //operator Real() const	too easy to misuse accidentally
     Real real() const
-	{
+        {
         if(sign_ == 0) return 0;
 #ifdef DEBUG
         if(lognum_ > maxlogdouble)
-        { 
+            { 
             Print(lognum_);
-	    throw TooBigForReal();
+            throw TooBigForReal();
             //Error("LogNumber too big to convert to Real"); 
-        }
+            }
         if(lognum_ < -maxlogdouble)
-        { 
+            { 
             Print(lognum_);
-	    throw TooSmallForReal();
+            throw TooSmallForReal();
             //Error("LogNumber too small to convert to Real"); 
-        }
+            }
 #endif
         return sign_ * exp(lognum_);
-	}
+        }
 
-    inline bool operator==(const LogNumber& other) const
-    { return (sign_ == other.sign_) && (lognum_ == other.lognum_); }
+    inline bool 
+    operator==(const LogNumber& other) const
+        { return (sign_ == other.sign_) && (lognum_ == other.lognum_); }
 
-    inline bool approxEquals(const LogNumber& other) const
-    { return (sign_ == other.sign_) && (fabs(lognum_-other.lognum_) < LogNumber_Accuracy); }
+    inline bool 
+    approxEquals(const LogNumber& other) const
+        { return (sign_ == other.sign_) && (fabs(lognum_-other.lognum_) < LogNumber_Accuracy); }
 
-    LogNumber& operator*=(const LogNumber& other)
-	{
+    LogNumber& 
+    operator*=(const LogNumber& other)
+        {
         sign_ *= other.sign_;
         lognum_ += other.lognum_;
         return *this;
-	}
+        }
 
-    LogNumber& operator*=(Real other)
-	{
-        if(other == -1)	// special case for efficiency
+    LogNumber& 
+    operator*=(Real other)
         {
+        if(other == -1)	// special case for efficiency
+            {
             sign_ *= -1;
             return *this;
-        }
+            }
         return *this *= LogNumber(other);
-	}
+        }
 
-    LogNumber& operator/=(const LogNumber& other)
-	{
+    LogNumber& 
+    operator/=(const LogNumber& other)
+        {
         DO_IF_DEBUG(if(other.sign_ == 0) Error("divide by zero in LogNumber");)
         sign_ *= other.sign_;
         lognum_ -= other.lognum_;
         return *this;
-	}
+        }
 
-    LogNumber& operator/=(Real other) { return *this /= LogNumber(other); }
+    LogNumber& 
+    operator/=(Real other) { return *this /= LogNumber(other); }
 
-    LogNumber operator-() const 
+    LogNumber 
+    operator-() const 
         { LogNumber res(*this); res.sign_ *= -1; return res; }
 
-    LogNumber operator/(const LogNumber& other) const
+    LogNumber 
+    operator/(const LogNumber& other) const
         { LogNumber res(*this); res /= other; return res; }
-    LogNumber operator*(const LogNumber& other) const
+    LogNumber 
+    operator*(const LogNumber& other) const
         { LogNumber res(*this); res *= other; return res; }
 
-    bool operator<(const LogNumber& other) const
-	{
+    bool 
+    operator<(const LogNumber& other) const
+        {
         if(sign_ != other.sign_)
             { return sign_ < other.sign_; }
         else if(sign_ == 0)
@@ -140,26 +169,29 @@ public:
         else if(sign_ > 0)
             { return lognum_ < other.lognum_; }
         return lognum_ > other.lognum_;
-	}
+        }
 
-    bool magnitudeLessThan(const LogNumber& other) const
-	{
+    bool 
+    magnitudeLessThan(const LogNumber& other) const
+        {
         if(sign_ == 0) return other.sign_ != 0;
         if(other.sign_ == 0) return false;
         return lognum_ < other.lognum_;
-	}
+        }
 
-    friend inline std::ostream& operator<<(std::ostream& s, const LogNumber& N)
-    {
+    friend inline std::ostream& 
+    operator<<(std::ostream& s, const LogNumber& N)
+        {
         s << "LogNumber(" << N.logNum() << ",";
         if(N.sign() == 0) s << "0)";
         else           s << (N.sign() > 0 ? "+)" : "-)");
         return s;
-    }
+        }
 
 };
 
-inline LogNumber sqrt(const LogNumber& L)
+inline LogNumber 
+sqrt(const LogNumber& L)
 {
     if(L.sign() < 0) 
         Error("Negative LogNumber in sqrt");
