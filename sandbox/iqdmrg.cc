73ae17b739634c5e162e89503a08e210a50011a8
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2012-01-21 01:46:37 -0800

    Sandbox iqdmrg code now uses newer Heisenberg MPO builder.

--- sandbox/iqdmrg.cc
+++ sandbox/iqdmrg.cc
@@ -1,6 +1,6 @@
 #define THIS_IS_MAIN
 #include "core.h"
-#include "hams.h"
+#include "hams/heisenberg.h"
 using boost::format;
 using std::cout;
 using std::cerr;
@@ -9,14 +9,14 @@ using std::endl;
 int main(int argc, char* argv[])
 {
     int N = 100;
-    int nsweep = 5; 
+    int nsweep = 5;
     int minm = 1;
     int maxm = 100;
     Real cutoff = 1E-5;
 
     SpinOne::Model model(N);
 
-    IQMPO H = SpinOne::Heisenberg(model)();
+    IQMPO H = Heisenberg(model);
 
     InitState initState(N);
     for(int i = 1; i <= N; ++i) initState(i) = (i%2==1 ? model.Up(i) : model.Dn(i));
@@ -26,6 +26,7 @@ int main(int argc, char* argv[])
     cout << format("Initial energy = %.5f\n")%psiHphi(psi,H,psi);
 
     Sweeps sweeps(Sweeps::ramp_m,nsweep,minm,maxm,cutoff);
+
     Real En = dmrg(psi,H,sweeps);
 
     cout << format("\nGround State Energy = %.10f\n")%En;
