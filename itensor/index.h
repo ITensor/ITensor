676f82c71476783c96fd771cc7c89635b4afe0c6
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2012-07-21 13:20:27 -0700

    Cleaned up documentation for index.h

--- itensor/index.h
+++ itensor/index.h
@@ -127,13 +127,13 @@ struct IndexVal;
 
 
 
-///
-/// Index: represents a tensor index of fixed bond dimension m.
-/// The == operator can be used to determine if two Index's match
-/// (copies of the same Index instance).
-/// An Index can be made temporarily distinct from other copies 
-/// by increasing its "primelevel".
-///
+//
+// Index: represents a tensor index of fixed bond dimension m.
+// The == operator can be used to determine if two Index's match
+// (copies of the same Index instance).
+// An Index can be made temporarily distinct from other copies 
+// by increasing its "primelevel".
+//
 class Index
     {
     public:
@@ -141,28 +141,20 @@ class Index
     // Constructors
     //
 
-    /// Default constructor.
-    /// For a default constructed Index, isNull() returns true.
+    // For a default constructed Index, isNull() returns true.
     Index();
 
-    /// Standard Index constructor.
-    /// @param name Name of Index for printing purposes
-    /// @param mm   Bond dimension (default = 1)
-    /// @param it   IndexType of Index (Link, Site) (default = Link)
-    /// @param plev Prime level (default = 0)
+    // Standard Index constructor.
+    // Name of Index is used for printing purposes
     Index(const std::string& name, 
-          int mm = 1, 
-          IndexType it=Link, 
-          int plev = 0);
+          int m = 1, 
+          IndexType it = Link, 
+          int primelev = 0);
 
-    /// Input stream constructor.
-    /// @param s Binary input stream
+    // Input stream constructor.
     Index(std::istream& s) { read(s); }
 
-    /// Primelevel copy constructor.
-    /// @param pt       PrimeType to use
-    /// @param other    Index to copy and prime
-    /// @param primeinc Incremement amount (default = 1)
+    // Copy constructor which increments the primelevel of the copy.
     Index(PrimeType pt,
           const Index& other, 
           int primeinc = 1);
@@ -171,71 +163,58 @@ class Index
     // Accessor Methods
     //
 
-    /// Returns the bond dimension of an Index.
-    /// @return Bond dimension of this Index
+    // Returns the bond dimension of an Index.
     int 
     m() const;
 
-    /// Returns the unique id (uuid) of this Index.
-    /// @return Unique id of this Index.
+    // Returns the unique id (uuid) of this Index.
     const boost::uuids::uuid&
     Ind() const;
 
-    /// Returns the IndexType of this Index.
-    /// @return IndexType of this Index
+    // Returns the IndexType of this Index.
     IndexType 
     type() const;
 
-    /// Returns the name of this Index.
-    /// @return Name of this Index
+    // Returns the name of this Index.
     std::string 
     name() const;
 
-    /// Returns the name of this Index with primes removed.
-    /// @return Name of this Index without primes
+    // Returns the name of this Index with primes removed.
     const std::string&
     rawname() const;
 
-    /// Sets the name of this Index.
+    // Change the name of this Index.
     void 
     setname(const std::string& newname);
 
-    /// Returns a string version of this Index's bond dimension.
-    /// @return String version of bond dimension
+    // Returns a string version of this Index's bond dimension.
     std::string 
     showm() const;
 
-    /// Returns a unique Real number identifying this Index.
-    /// Useful for rapidly checking that two Index instances match.
-    /// @return Unique Real id number
+    // Returns a unique Real number identifying this Index.
+    // Useful for rapidly checking that two Index instances match.
     Real 
     uniqueReal() const;
 
-    /// Returns true if Index was default initialized.
-    /// @return True if default initialized
+    // Returns true if Index was default initialized.
     bool 
     isNull() const;
-    /// Returns true if Index was NOT default initialized.
-    /// @return True if not default initialized
+    // Returns true if Index was NOT default initialized.
     bool 
     isNotNull() const;
 
-    /// Returns the number of copies of this Index.
-    /// @return Number of copies of Index
+    // Returns the number of copies of this Index.
     int 
     count() const;
 
-    /// Returns the prime level of this Index
-    /// @return Prime level
+    // Returns the prime level of this Index
     int 
     primeLevel() const;
-    /// Sets the prime level to a specified value.
-    /// @param plev New prime level
+    // Set the prime level to a specified value.
     void 
     primeLevel(int plev);
 
-    /// Returns the Arrow direction of this Index.
-    /// @return Arrow direction
+    // Returns the Arrow direction of this Index.
     Arrow 
     dir() const { return Out; }
 
@@ -243,31 +222,26 @@ class Index
     // Operators
     //
 
-    /// Equality (==) operator.
-    /// Evaluates to true if other Index
-    /// is a copy of this Index with the same
-    /// primelevel.
+    // Equality (==) operator.
+    // Evaluates to true if other Index is a
+    // copy of this Index with the same
+    // primelevel.
     bool 
     operator==(const Index& other) const;
 
-    /// Check if other Index is a copy of this, ignoring primelevel.
+    // Check if other Index is a copy of this, ignoring primelevel.
     bool 
     noprime_equals(const Index& other) const;
 
-    /// Less than (<) operator.
-    /// Useful for sorting Index objects.
+    // Less than (<) operator.
+    // Useful for sorting Index objects.
     bool 
     operator<(const Index& other) const;
 
-    /// Creates an IndexVal from this Index.
-    /// @param i The index value to use
-    /// @return An IndexVal instance referring to the ith value of this Index
+    // Creates an IndexVal from this Index with index i.
     IndexVal operator()(int i) const;
 
-    /// Output stream (<<) operator.
-    /// @param s Standard output stream
-    /// @param t Index object
-    /// @return Reference to modified output stream
+    // Output stream (<<) operator.
     friend std::ostream& 
     operator<<(std::ostream & s, const Index &t);
 
@@ -275,37 +249,28 @@ class Index
     // Prime methods
     //
 
-    /// Switch primelevel from plevold to plevnew. 
-    /// @param plevold Old primelevel
-    /// @param plevnew New primelevel
-    /// @param pt      Type of Index to modify (default = primeBoth)
+    // Switch primelevel from plevold to plevnew. 
+    // Has no effect if plevold doesn't match current primelevel.
     void 
     mapprime(int plevold, int plevnew, PrimeType pt = primeBoth);
 
-    /// Switch primelevel from plevold to plevnew. 
-    /// @param pt      Type of Index to modify
-    /// @param inc     Primelevel increment amount (default = 1)
+    // Increment primelevel by 1 (or optionally by amount inc).
     void 
-    doprime(PrimeType pt, int inc = 1);
+    doprime(PrimeType pt = primeBoth, int inc = 1);
 
-    /// Return copy of this Index, increasing primelevel.
-    /// @param inc     Primelevel increment amount (default = 1)
+    // Return copy of this Index, increasing primelevel.
     Index 
     primed(int inc = 1) const { return Index(primeBoth,*this,inc); }
 
-    /// Make a copy of this Index, increasing primelevel.
-    /// @param inc     Primelevel increment amount (default = 1)
-    /// @return Copy of this Index with increased primelevel
+    // Make a copy of this Index, increasing primelevel.
     Index friend inline
     primed(const Index& I, int inc = 1) { return Index(primeBoth,I,inc); }
 
-    /// Return a copy of this Index with primelevel set to zero.
-    /// @return Copy of this Index with zero primelevel
+    // Return a copy of this Index with primelevel set to zero.
     Index 
     deprimed() const;
 
-    /// Set primelevel to zero.
-    /// @param pt Type of Index to modify (default = primeBoth)
+    // Set primelevel to zero.
     void 
     noprime(PrimeType pt = primeBoth) { doprime(pt,-primelevel_); }
 
@@ -313,25 +278,22 @@ class Index
     // Other methods
     //
 
-    /// Write Index to binary output stream.
-    /// @param s Output stream for writing
+    // Write Index to binary output stream.
     void 
     write(std::ostream& s) const;
 
-    /// Read Index from binary input stream.
-    /// @param s Input stream for reading
+    // Read Index from binary input stream.
     void 
     read(std::istream& s);
 
-    /// Print Index to stdout.
-    /// @param name Name of Index variable (default = "")
+    // Print Index to stdout.
     void 
     print(const std::string& name = "") const
         { std::cout << "\n" << name << " =\n" << *this << std::endl; }
 
-    /// Conjugate this Index.
-    /// Currently has no effect; for forward compatibility
-    /// with Arrows and interface compatibility with class IQIndex.
+    // Conjugate this Index.
+    // Currently has no effect; for forward compatibility
+    // with Arrows and interface compatibility with class IQIndex.
     void 
     conj() { } //for forward compatibility with arrows
 
@@ -342,18 +304,21 @@ class Index
         return Null_;
         }
 
+    // Static Index indexing real and imaginary parts of a complex ITensor.
     static const Index& IndReIm()
         {
         static const Index IndReIm_(makeReIm);
         return IndReIm_;
         }
 
+    // IndReIm with primelevel 1
     static const Index& IndReImP()
         {
         static const Index IndReImP_(makeReImP);
         return IndReImP_;
         }
 
+    // IndReIm with primelevel 2
     static const Index& IndReImPP()
         {
         static const Index IndReImPP_(makeReImPP);
@@ -383,9 +348,8 @@ class Index
 
 //
 // IndexDat
+// Storage for Index objects.
 //
-
-//Storage for Indexes
 class IndexDat
     {
     public:
@@ -478,8 +442,9 @@ class IndexDat
 
 //
 // IndexVal
+// Represents an Index set to a specific value
+// from 1 to its maximum range m.
 //
-
 struct IndexVal
     {
     Index ind; 
@@ -489,13 +454,6 @@ struct IndexVal
 
     IndexVal(const Index& index, int i_);
 
-    static const IndexVal& 
-    Null()
-        {
-        static const IndexVal Null_(makeNull);
-        return Null_;
-        }
-
     bool 
     operator==(const IndexVal& other) const; 
 
@@ -504,6 +462,13 @@ struct IndexVal
 
     friend std::ostream& 
     operator<<(std::ostream& s, const IndexVal& iv);
+
+    static const IndexVal& 
+    Null()
+        {
+        static const IndexVal Null_(makeNull);
+        return Null_;
+        }
 
     private:
 
