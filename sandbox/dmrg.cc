f2c63ee99263c5a06f667f3eb5ff826c12926aab
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2012-07-31 19:26:41 -0700

    Updated sample/sandbox DMRG codes with newer library usage and many more comments.

--- sandbox/dmrg.cc
+++ sandbox/dmrg.cc
@@ -8,29 +8,53 @@ using namespace std;
 int main(int argc, char* argv[])
     {
     int N = 100;
-    int nsweep = 5; 
-    int minm = 1;
-    int maxm = 200;
-    Real cutoff = 1E-8;
 
-    //SpinHalf model(N);
-    SpinOne model(N);
+    //
+    // Initialize the site degrees of freedom.
+    //
+    SpinOne model(N);    // make a chain of N spin 1's
+    //SpinHalf model(N); // make a chain of N spin 1/2's
 
+    //
+    // Create the Hamiltonian matrix product operator (MPO)
+    //
     MPO H = Heisenberg(model);
 
+    //
+    // Set the initial wavefunction matrix product state (MPS)
+    // to be a Neel state.
+    //
     InitState initState(N);
     for(int i = 1; i <= N; ++i) 
         initState(i) = (i%2==1 ? model.Up(i) : model.Dn(i));
 
     MPS psi(model,initState);
 
-    cout << format("Initial energy = %.5f")%psiHphi(psi,H,psi) << endl;
+    //
+    // psiHphi calculates matrix elements of MPO's with respect to MPS's
+    // psiHphi(psi,H,psi) = <psi|H|psi>
+    //
+    cout << format("Initial energy = %.5f") % psiHphi(psi,H,psi) << endl;
 
-    Sweeps sweeps(Sweeps::exp_m,nsweep,minm,maxm,cutoff);
+    //
+    // Set the parameters controlling the accuracy of the DMRG
+    // calculation for each DMRG sweep. Here less than 10 maxm
+    // values are provided, so all remaining sweeps will use the
+    // last maxm (= 200).
+    //
+    Sweeps sweeps(10);
+    sweeps.maxm() = 50,50,100,100,200;
+    sweeps.cutoff() = 1E-10;
     cout << sweeps;
 
-    Real En = dmrg(psi,H,sweeps);
+    //
+    // Begin the DMRG calculation
+    //
+    Real En = dmrg(psi,H,sweeps,Quiet());
 
+    //
+    // Print the final energy reported by DMRG
+    //
     cout << format("\nGround State Energy = %.10f")%En << endl;
 
     return 0;
--- sandbox/iqdmrg.cc
+++ sandbox/iqdmrg.cc
@@ -8,30 +8,58 @@ using namespace std;
 int main(int argc, char* argv[])
     {
     int N = 100;
-    int nsweep = 5;
-    int minm = 1;
-    int maxm = 100;
-    Real cutoff = 1E-8;
 
-    //SpinHalf model(N);
+    //
+    // Initialize the site degrees of freedom.
+    //
     SpinOne model(N);
+    //SpinHalf model(N);
 
+    //
+    // Create the Hamiltonian matrix product operator.
+    // Here we use the IQMPO class which is an MPO of 
+    // IQTensors, tensors whose indices are sorted
+    // with respect to quantum numbers
+    //
     IQMPO H = Heisenberg(model);
 
+    // Set the initial wavefunction matrix product state
+    // to be a Neel state.
+    //
     InitState initState(N);
     for(int i = 1; i <= N; ++i) 
         initState(i) = (i%2==1 ? model.Up(i) : model.Dn(i));
 
     IQMPS psi(model,initState);
 
+    //
+    // psiHphi calculates matrix elements of MPO's with respect to MPS's
+    // psiHphi(psi,H,psi) = <psi|H|psi>
+    //
     cout << format("Initial energy = %.5f\n")%psiHphi(psi,H,psi);
 
-    Sweeps sweeps(Sweeps::exp_m,nsweep,minm,maxm,cutoff);
+    //
+    // Set the parameters controlling the accuracy of the DMRG
+    // calculation for each DMRG sweep. Here less than 10 maxm
+    // values are provided, so all remaining sweeps will use the
+    // last maxm (= 200).
+    //
+    Sweeps sweeps(10);
+    sweeps.maxm() = 50,50,100,100,200;
+    sweeps.cutoff() = 1E-8;
+    cout << sweeps;
 
-    Real En = dmrg(psi,H,sweeps);
+    //
+    // Begin the DMRG calculation
+    //
+    Real En = dmrg(psi,H,sweeps,Quiet());
 
+    //
+    // Print the final energy reported by DMRG
+    //
     cout << format("\nGround State Energy = %.10f\n")%En;
     cout << format("\nUsing psiHphi = %.10f\n") % psiHphi(psi,H,psi);
+
     cout << "\nTotal QN of Ground State = " << totalQN(psi) << "\n";
 
     return 0;
