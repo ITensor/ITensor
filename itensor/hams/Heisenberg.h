52a2e130f7d42f8a2c5437706b0095743cf8a70b
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2012-12-14 11:28:05 -0800

    Changed Heisenberg MPO builder to no longer subclass deprecated MPOBuilder, accept an OptSet instead of individual parameters.

--- itensor/hams/Heisenberg.h
+++ itensor/hams/Heisenberg.h
@@ -4,156 +4,150 @@
 //
 #ifndef __ITENSOR_HAMS_HEISENBERG_H
 #define __ITENSOR_HAMS_HEISENBERG_H
+
 #include "../mpo.h"
-#include "../hams.h"
 
-class Heisenberg : public MPOBuilder
-{
-public:
-    typedef MPOBuilder Parent;
+#define Cout std::cout
+#define Endl std::endl
+#define Format boost::format
 
-    Heisenberg(const Model& model_);
+class Heisenberg
+    {
+    public:
 
-    Heisenberg(const Model& model_, int ny);
-
-    Heisenberg(const Model& model_, int ny, Real boundary_h);
+    Heisenberg(const Model& model, 
+               const OptSet& opts = Global::opts());
 
     Real
     J() const { return J_; }
-
     void
-    J(Real val) { initted = false; J_ = val; }
+    J(Real val) { initted_ = false; J_ = val; }
 
     int
-    ny() const { return ny_; }
-
+    Ny() const { return Ny_; }
     void
-    ny(int val) { initted = false; ny_ = val; }
+    Ny(int val) { initted_ = false; Ny_ = val; }
+
+    Real
+    Boundary_h() const { return Boundary_h_; }
+    void
+    Boundary_h(Real val) { initted_ = false; Boundary_h_ = val; }
 
     operator MPO() { init_(); return H; }
 
     operator IQMPO() { init_(); return H; }
 
-private:
+    private:
 
-    const Model& model;
-    int ny_,nx_;
-    Real J_, boundary_h_;
-    bool initted;
+    //////////////////
+    //
+    // Data Members
+
+    const Model& model_;
+    int Ny_,
+        Nx_;
+    Real J_, 
+         Boundary_h_;
+    bool initted_;
     MPO H;
 
-    void init_();
+    //
+    //////////////////
 
-}; //class Heisenberg
+    void 
+    init_();
 
-inline Heisenberg::
-Heisenberg(const Model& model_) 
-    : Parent(model_), 
-      model(model_), 
-      ny_(1),
-      nx_(this->Ns),
-      J_(1), 
-      boundary_h_(0),
-      initted(false)
-    { }
+    }; //class Heisenberg
 
 inline Heisenberg::
-Heisenberg(const Model& model_, int ny) 
-    : Parent(model_), 
-      model(model_), 
-      ny_(ny),
-      nx_(this->Ns/ny_),
-      J_(1), 
-      boundary_h_(0),
-      initted(false)
-    { }
-
-inline Heisenberg::
-Heisenberg(const Model& model_, int ny, Real boundary_h) 
-    : Parent(model_), 
-      model(model_), 
-      ny_(ny),
-      nx_(this->Ns/ny_),
-      J_(1), 
-      boundary_h_(boundary_h),
-      initted(false)
-    { }
+Heisenberg(const Model& model, const OptSet& opts)
+    : 
+    model_(model), 
+    initted_(false)
+    { 
+    Ny_ = opts.intOrDefault("Ny",1);
+    Nx_ = model_.NN()/Ny_;
+    J_ = opts.realOrDefault("J",1.);
+    Boundary_h_ = opts.realOrDefault("Boundary_h",0.);
+    }
 
 void inline Heisenberg::
 init_()
     {
-    if(initted) return;
+    if(initted_) return;
 
-    H = MPO(model);
+    H = MPO(model_);
 
+    const int Ns = model_.NN();
     const int nop = 3;
-    const int max_mpo_dist = ny_;
+    const int max_mpo_dist = Ny_;
     const int k = 5+nop*(max_mpo_dist-1);
 
-    std::vector<Index> links(Ns+1);
+    std::vector<Index> links(model_.NN()+1);
     for(int l = 0; l <= Ns; ++l) links.at(l) = Index(nameint("hl",l),k);
-
-    ITensor W;
 
     for(int n = 1; n <= Ns; ++n)
         {
         ITensor& W = H.AAnc(n);
         Index &row = links[n-1], &col = links[n];
 
-        W = ITensor(model.si(n),model.siP(n),row,col);
+        W = ITensor(model_.si(n),model_.siP(n),row,col);
 
-        W += model.id(n) * row(1) * col(1);
-        W += model.id(n) * row(k) * col(k);
+        W += model_.id(n) * row(1) * col(1);
+        W += model_.id(n) * row(k) * col(k);
 
-        W += model.sz(n) * row(2) * col(1);
-        W += model.sp(n) * row(3) * col(1);
-        W += model.sm(n) * row(4) * col(1);
+        W += model_.sz(n) * row(2) * col(1);
+        W += model_.sp(n) * row(3) * col(1);
+        W += model_.sm(n) * row(4) * col(1);
 
         //Horizontal bonds
-        int mpo_dist = ny_; 
-        W += model.sz(n) * row(k) * col(2+nop*(mpo_dist-1)) * J_;
-        W += model.sm(n) * row(k) * col(3+nop*(mpo_dist-1)) * J_/2;
-        W += model.sp(n) * row(k) * col(4+nop*(mpo_dist-1)) * J_/2;
+        int mpo_dist = Ny_; 
+        W += model_.sz(n) * row(k) * col(2+nop*(mpo_dist-1)) * J_;
+        W += model_.sm(n) * row(k) * col(3+nop*(mpo_dist-1)) * J_/2;
+        W += model_.sp(n) * row(k) * col(4+nop*(mpo_dist-1)) * J_/2;
 
         //Add boundary field if requested
-        const int x = (n-1)/ny_+1, y = (n-1)%ny_+1;
-        if(boundary_h_ != 0 && (x == 1 || x == nx_))
+        const int x = (n-1)/Ny_+1, y = (n-1)%Ny_+1;
+        if(Boundary_h_ != 0 && (x == 1 || x == Nx_))
             {
-            Real eff_h = boundary_h_;
+            Real eff_h = Boundary_h_;
             if(J_ > 0) eff_h *= (x%2==1 ? -1 : 1)*(y%2==1 ? -1 : 1);
             //cerr << format("Doing a staggered bf of %.2f at site %d (%d,%d)\n")%eff_h%n%x%y;
-            W += model.sz(n) * ITensor(row(k),col(1)) * eff_h;
+            W += model_.sz(n) * ITensor(row(k),col(1)) * eff_h;
             }
 
-        //The following only apply if ny_ > 1:
+        //The following only apply if Ny_ > 1:
 
         //String of identity ops
         for(int q = 1; q <= nop*(max_mpo_dist-1); ++q)
-            { W += model.id(n) * row(1+nop+q) * col(1+q); }
+            { W += model_.id(n) * row(1+nop+q) * col(1+q); }
 
         //Periodic BC bond
-        if(y == 1 && ny_ > 1)
+        if(y == 1 && Ny_ > 1)
             {
-            int mpo_dist = ny_-1; 
-            W += model.sz(n) * row(k) * col(2+nop*(mpo_dist-1)) * J_;
-            W += model.sm(n) * row(k) * col(3+nop*(mpo_dist-1)) * J_/2;
-            W += model.sp(n) * row(k) * col(4+nop*(mpo_dist-1)) * J_/2;
+            int mpo_dist = Ny_-1; 
+            W += model_.sz(n) * row(k) * col(2+nop*(mpo_dist-1)) * J_;
+            W += model_.sm(n) * row(k) * col(3+nop*(mpo_dist-1)) * J_/2;
+            W += model_.sp(n) * row(k) * col(4+nop*(mpo_dist-1)) * J_/2;
             }
 
         //N.N. bond along column
-        if(y != ny_)
+        if(y != Ny_)
             {
-            W += model.sz(n) * row(k) * col(2) * J_;
-            W += model.sm(n) * row(k) * col(3) * J_/2;
-            W += model.sp(n) * row(k) * col(4) * J_/2;
+            W += model_.sz(n) * row(k) * col(2) * J_;
+            W += model_.sm(n) * row(k) * col(3) * J_/2;
+            W += model_.sp(n) * row(k) * col(4) * J_/2;
             }
         }
 
-        H.AAnc(1) *= makeLedge(links.at(0));
-        H.AAnc(Ns) *= makeRedge(links.at(Ns));
+        H.AAnc(1) *= ITensor(links.at(0)(k));
+        H.AAnc(Ns) *= ITensor(links.at(Ns)(1));
 
-    initted = true;
+    initted_ = true;
     }
 
+#undef Cout
+#undef Endl
+#undef Format
 
 #endif
