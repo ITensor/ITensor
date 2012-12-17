3d4791bb483535b4c8d8f61e4c0888a99df0f801
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2012-12-16 23:29:52 -0500

    Added tutorial lesson 4 lab app j1j2.

---
+++ tutorial/04_four/j1j2.cc
@@ -0,0 +1,50 @@
+#include "j1j2.h"
+
+using namespace std;
+using boost::format;
+
+ITensor
+B(const Model& model, int b)
+    {
+    ITensor B_ = model.sz(b)*model.sz(b+1)
+              + 0.5*model.sp(b)*model.sm(b+1)
+              + 0.5*model.sm(b)*model.sp(b+1);
+    return B_;
+    }
+
+int
+main(int argc, char* argv[])
+    {
+
+    const int N = 20;
+
+    //Model objects represent a collection of 
+    //lattice degrees of freedom of a certain type
+    SpinHalf model(N);
+
+    vector<Real> J2s(1,NAN),
+                 dimer(1,NAN);
+
+    for(Real J2 = 0.0; J2 <= 0.5; J2 += 0.05)
+        {
+        //
+        // Compute ground state using black box
+        //
+        MPS psi = computeGroundState(model,J2);
+
+        Real val = 0;
+
+        J2s.push_back(J2);
+        dimer.push_back(val);
+        }
+
+    for(int j = 1; j < int(dimer.size()); ++j)
+        {
+        cout << format("%.5f %.10f")
+                % J2s.at(j)
+                % dimer.at(j) 
+                << endl;
+        }
+
+    return 0;
+    }
---
+++ tutorial/04_four/j1j2.h
@@ -0,0 +1,40 @@
+#ifndef __J1J2_H
+#define __J1J2_H
+
+#include "core.h"
+#include "model/spinhalf.h"
+#include "hams/J1J2Chain.h"
+
+#define Cout std::cout
+#define Endl std::endl
+#define Format boost::format
+
+
+MPS inline
+computeGroundState(const SpinHalf& model, Real J2)
+    {
+    MPO H = J1J2Chain(model,Opt("J2",J2));
+
+    MPS psi(model);
+
+    Sweeps sweeps(5);
+    sweeps.maxm() = 50,50,100,100,200;
+    sweeps.cutoff() = 1E-9;
+
+    Cout << Format("Starting ground state calculation for J2 = %.3f")  
+            % J2 << Endl;
+
+    dmrg(psi,H,sweeps,Quiet());
+
+    Cout << "Done with ground state calculation." << Endl;
+
+    return psi;
+    }
+
+
+
+#undef Cout
+#undef Endl
+#undef Format
+
+#endif
