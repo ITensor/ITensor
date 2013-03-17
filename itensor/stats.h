feed03b902271406c02da4217f5a0f30dce5412b
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2013-03-17 02:09:44 -0700

    Renamed measure.h to stats.h and renamed class Measure to class Stats. Also renamed member function ave to avg.

--- itensor/stats.h
+++ itensor/stats.h
@@ -2,23 +2,22 @@
 // Distributed under the ITensor Library License, Version 1.0.
 //    (See accompanying LICENSE file.)
 //
-#ifndef __ITENSOR_MEASURE_H
-#define __ITENSOR_MEASURE_H
-#include "global.h"
-#include "real.h"
-#include <vector>
+#ifndef __ITENSOR_STATS_H
+#define __ITENSOR_STATS_H
 
-class Measure
+#include "real.h"
+
+class Stats
     {
     public:
 
     std::vector<Real> dat;
-    Measure() { dat.reserve(100); }
+    Stats() { dat.reserve(100); }
 
     void putin(Real x) { dat.push_back(x); }
 
     Real 
-    ave() const
+    avg() const
         {
         if(dat.empty()) return 0;
         Real s = 0;
@@ -30,7 +29,7 @@ class Measure
     sigma() const
         {
         if(dat.size() < 2) return 0;
-        Real av = ave(), s = 0;
+        Real av = avg(), s = 0;
         for(size_t j = 0; j < dat.size(); ++j) s += sqr(dat.at(j)-av);
         return sqrt(s/dat.size());
         }
@@ -57,7 +56,7 @@ class Measure
             bin.el(i/bs) += dat.at(i);
             }
         bin *= 1.0/bs;
-        Measure bmeas;
+        Stats bmeas;
         for(int i = 1; i <= nbin; ++i) bmeas.putin(bin(i));
         return bmeas.err();
         }
@@ -65,7 +64,7 @@ class Measure
     Real 
     bootstrap(int nresamples = 1000)
         { //Uses the bootstrap procedure to estimate the std deviation
-        Measure boot;
+        Stats boot;
         int n = dat.size();
         if(n < 2) return 0.0;
         for(int sample = 1; sample <= nresamples; ++sample)
@@ -83,12 +82,12 @@ class Measure
         }
 
     Real 
-    bootstrap_2c(Real prefactor,Measure firstc, bool correlated = true, int nresamples = 1000)
+    bootstrap_2c(Real prefactor,Stats firstc, bool correlated = true, int nresamples = 1000)
         { //Uses the bootstrap procedure to estimate the second cumulant error
-        Measure boot2c;
+        Stats boot2c;
         Real n = dat.size();
         if(n < 2) return 0.0;
-        if(firstc.dat.size() != n) error("Measurements don't have the same size data sets.");
+        if(firstc.dat.size() != n) error("Stats instances don't have the same size data sets.");
         for(int sample = 1; sample <= nresamples; ++sample)
             {
             Real avg2 = 0.0;
