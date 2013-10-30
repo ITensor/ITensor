e8159e2f9b89dda223a167f3db5a47cc7d8f1947
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2013-10-30 15:45:12 -0400

    Moved templated gateTEvol method to header to allow general template arguments (especially any type of container for the gates).

--- itensor/tevol.h
+++ itensor/tevol.h
@@ -41,13 +41,75 @@ imagTEvol(const MPOt<Tensor>& H,
 // Options recognized:
 //     Verbose - print useful information to stdout
 //
-template <class Tensor>
+template <class Iterable, class Tensor>
 Real
-gateTEvol(const std::list<BondGate<Tensor> >& gatelist, 
+gateTEvol(const Iterable& gatelist, 
           Real ttotal, Real tstep, 
           MPSt<Tensor>& psi, 
           const OptSet& opts = Global::opts());
 
+
+
+//
+//
+// Implementations
+//
+//
+
+template <class Iterable, class Tensor>
+Real
+gateTEvol(const Iterable& gatelist, Real ttotal, Real tstep, 
+          MPSt<Tensor>& psi, 
+          const OptSet& opts)
+    {
+    bool verbose = opts.getBool("Verbose",false);
+
+    const int nt = int(ttotal/tstep+(1e-9*(ttotal/tstep)));
+    if(fabs(nt*tstep-ttotal) > 1E-9)
+        {
+        Error("Timestep not commensurate with total time");
+        }
+
+    Real tsofar = 0;
+    Real tot_norm = psi.normalize();
+    if(verbose) 
+        {
+        Cout << Format("Taking %d steps of timestep %.5f, total time %.5f")
+                % nt
+                % tstep
+                % ttotal
+                << Endl;
+        }
+    for(int tt = 1; tt <= nt; ++tt)
+        {
+        Foreach(const BondGate<Tensor>& G, gatelist)
+            {
+            psi.position(G.i());
+            psi.applygate(G);
+            }
+
+        if(verbose)
+            {
+            Real percentdone = (100.*tt)/nt;
+            if(percentdone < 99.5 || (tt==nt))
+                {
+                Cout << Format("\b\b\b%2.f%%") % percentdone;
+                Cout.flush();
+                }
+            }
+
+        tot_norm *= psi.normalize();
+
+        tsofar += tstep;
+        }
+    if(verbose) 
+        {
+        Cout << Format("\nTotal time evolved = %.5f\n") % tsofar << Endl;
+        }
+
+    return tot_norm;
+
+    } // gateTEvol
 
 #undef Cout
 #undef Endl
