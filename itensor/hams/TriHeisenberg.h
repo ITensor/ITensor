e37b20438d25e96980e97324c3dc822dace934e0
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2014-03-10 16:55:40 -0500

    Updated TriHeisenberg Hamiltonian to use new Model interface.

--- itensor/hams/TriHeisenberg.h
+++ itensor/hams/TriHeisenberg.h
@@ -80,18 +80,18 @@ init_()
 
         W = ITensor(model_.si(n),model_.siP(n),row,col);
 
-        W += model_.id(n) * row(1) * col(1);
-        W += model_.id(n) * row(k) * col(k);
+        W += model_.op("Id",n) * row(1) * col(1);
+        W += model_.op("Id",n) * row(k) * col(k);
 
-        W += model_.sz(n) * row(2) * col(1);
-        W += model_.sp(n) * row(3) * col(1);
-        W += model_.sm(n) * row(4) * col(1);
+        W += model_.op("Sz",n) * row(2) * col(1);
+        W += model_.op("S+",n) * row(3) * col(1);
+        W += model_.op("S-",n) * row(4) * col(1);
 
         //Horizontal bonds, connect n -> n+Ny_
         int mpo_dist = Ny_; 
-        W += model_.sz(n) * row(k) * col(2+nop*(mpo_dist-1)) * J_;
-        W += model_.sm(n) * row(k) * col(3+nop*(mpo_dist-1)) * J_/2;
-        W += model_.sp(n) * row(k) * col(4+nop*(mpo_dist-1)) * J_/2;
+        W += model_.op("Sz",n) * row(k) * col(2+nop*(mpo_dist-1)) * J_;
+        W += model_.op("S-",n) * row(k) * col(3+nop*(mpo_dist-1)) * J_/2;
+        W += model_.op("S+",n) * row(k) * col(4+nop*(mpo_dist-1)) * J_/2;
 
         //Add boundary field if requested
         const int x = (n-1)/Ny_+1, y = (n-1)%Ny_+1;
@@ -100,37 +100,37 @@ init_()
             Real eff_h = boundary_h_;
             eff_h *= ((x+y-2)%3==0 ? -1 : 0.5);
             std::cout << boost::format("Applying a pinning field of %.2f at site %d (%d,%d)\n")%eff_h%n%x%y;
-            W += model_.sz(n) * ITensor(row(k),col(1)) * eff_h;
+            W += model_.op("Sz",n) * ITensor(row(k),col(1)) * eff_h;
             }
 
         //The following only apply if Ny_ > 1:
 
         //String of identity ops
         for(int q = 1; q <= nop*(max_mpo_dist-1); ++q)
-            { W += model_.id(n) * row(1+nop+q) * col(1+q); }
+            { W += model_.op("Id",n) * row(1+nop+q) * col(1+q); }
 
         //Square lattice periodic BC bond
         //Connects n -> n+(Ny_-1)
         if(y == 1)
             {
             mpo_dist = Ny_-1; 
-            W += model_.sz(n) * row(k) * col(2+nop*(mpo_dist-1)) * J_;
-            W += model_.sm(n) * row(k) * col(3+nop*(mpo_dist-1)) * J_/2;
-            W += model_.sp(n) * row(k) * col(4+nop*(mpo_dist-1)) * J_/2;
+            W += model_.op("Sz",n) * row(k) * col(2+nop*(mpo_dist-1)) * J_;
+            W += model_.op("S-",n) * row(k) * col(3+nop*(mpo_dist-1)) * J_/2;
+            W += model_.op("S+",n) * row(k) * col(4+nop*(mpo_dist-1)) * J_/2;
             }
 
         //N.N. bond along column
-        W += model_.sz(n) * row(k) * col(2) * J_;
-        W += model_.sm(n) * row(k) * col(3) * J_/2;
-        W += model_.sp(n) * row(k) * col(4) * J_/2;
+        W += model_.op("Sz",n) * row(k) * col(2) * J_;
+        W += model_.op("S-",n) * row(k) * col(3) * J_/2;
+        W += model_.op("S+",n) * row(k) * col(4) * J_/2;
 
         //Diagonal bonds
         if(y != Ny_)
             {
             mpo_dist = Ny_+1; 
-            W += model_.sz(n) * row(k) * col(2+nop*(mpo_dist-1)) * J_;
-            W += model_.sm(n) * row(k) * col(3+nop*(mpo_dist-1)) * J_/2;
-            W += model_.sp(n) * row(k) * col(4+nop*(mpo_dist-1)) * J_/2;
+            W += model_.op("Sz",n) * row(k) * col(2+nop*(mpo_dist-1)) * J_;
+            W += model_.op("S-",n) * row(k) * col(3+nop*(mpo_dist-1)) * J_/2;
+            W += model_.op("S+",n) * row(k) * col(4+nop*(mpo_dist-1)) * J_/2;
             }
         }
 
