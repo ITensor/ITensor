60fcd4074b02aedc2557cc989f9a09ca6079bb6c
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2014-05-28 17:55:41 -0400

    Updated 02_two_site/imag_tevol code to use newer conventions; added some more comments.

--- tutorial/02_two_site/imag_tevol.cc
+++ tutorial/02_two_site/imag_tevol.cc
@@ -5,34 +5,34 @@ using namespace itensor;
 ITensor
 makeId(const Index& s)
     {
-    ITensor Id(s,primed(s));
-    Id(s(1),primed(s)(1)) = 1;
-    Id(s(2),primed(s)(2)) = 1;
+    ITensor Id(s,prime(s));
+    Id(s(1),prime(s)(1)) = 1;
+    Id(s(2),prime(s)(2)) = 1;
     return Id;
     }
 
 ITensor
 makeSp(const Index& s)
     {
-    ITensor Sp(s,primed(s));
-    Sp(s(2),primed(s)(1)) = 1;
+    ITensor Sp(s,prime(s));
+    Sp(s(2),prime(s)(1)) = 1;
     return Sp;
     }
 
 ITensor
 makeSm(const Index& s)
     {
-    ITensor Sm(s,primed(s));
-    Sm(s(1),primed(s)(2)) = 1;
+    ITensor Sm(s,prime(s));
+    Sm(s(1),prime(s)(2)) = 1;
     return Sm;
     }
 
 ITensor
 makeSz(const Index& s)
     {
-    ITensor Sz(s,primed(s));
-    Sz(s(1),primed(s)(1)) =  0.5;
-    Sz(s(2),primed(s)(2)) = -0.5;
+    ITensor Sz(s,prime(s));
+    Sz(s(1),prime(s)(1)) =  0.5;
+    Sz(s(2),prime(s)(2)) = -0.5;
     return Sz;
     }
 
@@ -51,14 +51,9 @@ main(int argc, char* argv[])
     ITensor psi(s1,s2); //default initialized to zero
 
     psi.randomize();
-
-    println("psi.norm() = ",psi.norm());
-    
     psi *= 1./psi.norm();
 
-    println("psi.norm() = ",psi.norm());
-
-    PrintData(psi);
+    //PrintData(psi);
 
     //
     // Single-site operators
@@ -84,32 +79,46 @@ main(int argc, char* argv[])
     // Energy expectation value
     //
 
-    ITensor cpsi = dag(primed(psi));
+    ITensor cpsi = dag(prime(psi));
     Real initEn = (cpsi * H * psi).toReal();
 
-    printfln("Initial energy = %.10f",initEn);
+    printfln("\nInitial energy = %.10f",initEn);
 
     //
     // Exponentiate H to form exp(-beta*H/2)
     // 
-    // Use formula:
+    // Use the formula:
     // exp(x) = 1 + x + x^2/2! + x^3/3! + ...
     //        = 1 + x * (1 + x/2 * (1 + x/3 * (...)))
     //        ~ ((x/3 + 1) * x/2 + 1) * x + 1
+    //
+    // to build up exp(x) in the for loop below.
 
-    const Real beta = 3;
+    const Real beta = 10;
     const int max_order = 100;
 
     const ITensor I = Id1*Id2;
 
     ITensor x = H*(-beta/2.);
 
-    ITensor expH = x;
+    //Make expH = (x/M + 1), similar to the innermost
+    //parentheses in the formula above:
+    ITensor expH = x/max_order + I;
 
     x.mapprime(1,2);
     x.mapprime(0,1);
 
-    for(int ord = max_order; ord >= 1; --ord)
+    //
+    // The tensor x now looks like:
+    //
+    //    s1''   s2''
+    //    |      |
+    //    ========
+    //    |      |
+    //    s1'    s1'
+    //
+
+    for(int ord = max_order-1; ord >= 1; --ord)
         {
         //
         //
@@ -122,7 +131,7 @@ main(int argc, char* argv[])
     psi_beta.noprime();
     psi_beta *= 1./psi_beta.norm();
 
-    Real En = (dag(primed(psi_beta)) * H * psi_beta).toReal();
+    Real En = (dag(prime(psi_beta)) * H * psi_beta).toReal();
     printfln("Energy at beta = %.3f: %.10f",beta,En);
 
     ITensor A(s1),B(s2),D;
