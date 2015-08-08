02f08fd3a30bc811b2878322044abb364fb9005b
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2015-08-08 15:45:33 -0400

    Changed tensor library typedefs and class names; TenRef can own its range
    Template classes are Ten<T,RangeT> and TenRef<T,RangeT>. Specializations for Real values and RangeT==Range are called Tensor, TensorRef, TensorRefc.
    
    Now TenRef can optionally own its range object, which is crucial for slicing functionality.

--- itensor/tensor/ten.h
+++ itensor/tensor/ten.h
@@ -7,23 +7,43 @@
 
 #include "itensor/types.h"
 #include "itensor/tensor/range.h"
+#include "itensor/detail/gcounter.h"
 
 namespace itensor {
 
 template<typename T, typename RangeT>
-class Tensor;
+class Ten;
+template<typename T, typename RangeT>
+class TenRef;
+
+//Specialize to Real elements
+template<typename RangeT>
+using RTen     = Ten<Real,RangeT>;
+template<typename RangeT>
+using RTenRef  = TenRef<Real,RangeT>;
+template<typename RangeT>
+using RTenRefc = TenRef<const Real,RangeT>;
+
+//Specialize to Real elements, RangeT=Range
+using Tensor     = RTen<Range>;
+using TensorRef  = RTenRef<Range>;
+using TensorRefc = RTenRefc<Range>;
+
+//Specialize to Cplx elements
+//using CTen     = Ten<Cplx,RangeT>;
+//template<typename RangeT>
+//using CTenRef  = TenRef<Cplx,RangeT>;
+//template<typename RangeT>
+//using CTenRefc = TenRef<const Cplx,RangeT>;
+
+//Specialize to Cplx elements, RangeT=Range
+//using CTensor     = Tensor<Cplx,Range>;
+//using CTensorRef  = TenRef<Cplx,Range>;
+//using CTensorRefc  = TenRef<const Cplx,Range>;
+
 
 template<typename T, typename RangeT>
-class TensorRef;
-
-using Ten     = Tensor<Real,Range>;
-template<typename RangeT>
-using TenRef  = TensorRef<Real,RangeT>;
-template<typename RangeT>
-using TenRefc = TensorRef<const Real,RangeT>;
-
-template<typename T, typename RangeT = Range>
-class TensorRef
+class TenRef
     { 
     public:
     using iterator = T*;
@@ -34,33 +54,101 @@ class TensorRef
     using size_type = long;
     using range_type = RangeT;
     using tensor_type = std::conditional_t<std::is_const<T>::value,
-                                           const Tensor<value_type,range_type>,
-                                           Tensor<value_type,range_type>>;
+                                           const Ten<value_type,range_type>,
+                                           Ten<value_type,range_type>>;
     private:
     pointer pdata_ = nullptr;
     const range_type* prange_ = nullptr;
+    range_type range_;
     public:
 
-    TensorRef() { }
+    TenRef() { }
 
-    TensorRef(pointer pdat,
-              const range_type& range)
-        :
-        pdata_(pdat),
+    TenRef(pointer pdat,
+           range_type const& range)
+      : pdata_(pdat),
         prange_(&range)
+        { 
+        }
+
+    TenRef(pointer pdat,
+           range_type && range)
+      : pdata_(pdat),
+        range_(std::move(range))
+        { 
+        prange_ = &range_;
+        }
+
+    TenRef(pointer pdat,
+           const range_type* prange)
+      : pdata_(pdat),
+        prange_(prange)
         { }
 
-    TensorRef(tensor_type& t) { pointTo(t); }
 
-    TensorRef&
+    TenRef(TenRef const& t)
+        {
+        operator=(t);
+        }
+
+    TenRef&
+    operator=(TenRef const& t)
+        {
+        pdata_ = t.pdata_;
+        if(t.ownRange())
+            {
+            range_ = t.range_;
+            prange_ = &range_;
+            }
+        else
+            {
+            prange_ = t.prange_;
+            }
+        return *this;
+        }
+
+    TenRef(TenRef && t)
+        {
+        operator=(std::move(t));
+        }
+
+    TenRef&
+    operator=(TenRef && t)
+        {
+        pdata_ = t.pdata_;
+        if(t.ownRange())
+            {
+            range_ = std::move(t.range_);
+            prange_ = &range_;
+            }
+        else
+            {
+            prange_ = t.prange_;
+            }
+        return *this;
+        }
+
+    TenRef(tensor_type& t) { pointTo(t); }
+
+    TenRef&
     operator=(tensor_type& t) { pointTo(t); return *this; }
 
-    TensorRef(tensor_type&& t) = delete;
+    TenRef(tensor_type&& t) = delete;
 
-    TensorRef&
+    TenRef&
     operator=(tensor_type&& t) = delete;
 
-    operator TensorRef<const value_type,range_type>() const { return TensorRef<const value_type,range_type>(data(),range()); }
+    operator TenRef<const value_type,range_type>() const 
+        { 
+        if(ownRange())
+            {
+            return TenRef<const value_type,range_type>(data(),range_type{range_}); 
+            }
+        return TenRef<const value_type,range_type>(data(),&range()); 
+        }
+
+    bool
+    ownRange() const { return prange_ == &range_; }
 
     size_type
     r() const { return prange_->r(); }
@@ -76,21 +164,40 @@ class TensorRef
     size_type
     stride(size_type i) const { return prange_->stride(i); }
 
-    const range_type&
+    range_type const&
     range() const { return *prange_; }
 
     // direct access to data
     pointer
     data() const { return pdata_; }
+    
+    reference
+    operator()() const 
+        { 
+#ifdef DEBUG
+        if(r() != 0) throw std::runtime_error("No indices passed to rank > 0 TenRef");
+#endif
+        return *pdata_; 
+        }
 
     template <typename... Inds>
     reference
-    operator()(Inds&&... ii) const { return pdata_[ind(*prange_,std::forward<Inds>(ii)...)]; }
+    operator()(Inds&&... ii) const 
+        { 
+#ifdef DEBUG
+        if(sizeof...(ii) != r()) throw std::runtime_error("Wrong number of indices passed to TenRef");
+#endif
+        return pdata_[ind(*prange_,std::forward<Inds>(ii)...)]; 
+        }
+
+    template<typename Indices>
+    reference
+    el(Indices const& ii) const { return pdata_[ind(*prange_,ii)]; }
 
     void
     clear() { pdata_ = nullptr; prange_ = nullptr; }
 
-    //Currently assuming TensorRefs point to contiguous data:
+    //Currently assuming TenRefs point to contiguous data:
 
     iterator
     begin() const { return pdata_; }
@@ -111,14 +218,31 @@ class TensorRef
 
 template<typename T, typename RangeT>
 auto
-makeTensorRef(T* p,
-              const RangeT& range)
+makeTenRef(T* p,
+           const RangeT* prange)
     {
-    return TensorRef<T,RangeT>(p,range);
+    return TenRef<T,RangeT>(p,prange);
     }
 
-template<typename T, typename RangeT = Range>
-class Tensor
+template<typename T, typename RangeT>
+auto
+makeTenRef(T* p,
+           RangeT & range)
+    {
+    return TenRef<T,RangeT>(p,&range);
+    }
+
+template<typename T, typename RangeT,
+         class = std::enable_if_t<std::is_rvalue_reference<RangeT&&>::value>>
+auto
+makeTenRef(T* p,
+           RangeT && range)
+    {
+    return TenRef<T,RangeT>(p,std::move(range));
+    }
+
+template<typename T, typename RangeT>
+class Ten
     {
     public:
     using storage_type = std::vector<T>;
@@ -136,61 +260,64 @@ class Tensor
     storage_type data_;
     public:
 
-    Tensor() { }
+    Ten() { }
 
     template<typename... Dims>
-    Tensor(size_type d0, Dims&&... rest)
-        : range_(d0,std::forward<Dims>(rest)...)
+    Ten(size_type d0, Dims&&... rest)
+      : range_(d0,std::forward<Dims>(rest)...)
         {
         init();
         }
 
-    Tensor(const range_type& range)
-        : range_(range)
+    Ten(range_type const& range)
+      : range_(range)
         {
         init();
         }
 
-    Tensor(range_type&& range)
-        : range_(std::move(range))
+    Ten(range_type && range)
+      : range_(std::move(range))
         {
         init();
         }
 
-    Tensor(range_type&& range,
-           storage_type&& data)
-        : 
-        range_(std::move(range)),
+    Ten(range_type && range,
+        storage_type&& data)
+      : range_(std::move(range)),
         data_(std::move(data))
         { 
 #ifdef DEBUG
         auto len = area(range_);
-        if(len==0) throw std::runtime_error("Zero are in Tensor");
+        if(len==0) throw std::runtime_error("Zero area in Ten");
         if(data_.size() != size_t(len)) throw std::runtime_error("Wrong size of input data");
 #endif
         }
 
-    Tensor(const Tensor& other) { assignFrom(other); }
+    Ten(Ten const& other) { assignFrom(other); }
 
-    Tensor(Tensor&& other) { moveFrom(std::move(other)); }
+    Ten(Ten && other) { moveFrom(std::move(other)); }
 
-    Tensor&
-    operator=(const Tensor& other) { assignFrom(other); return *this; }
+    Ten&
+    operator=(Ten const& other) { assignFrom(other); return *this; }
 
-    Tensor& 
-    operator=(Tensor&& other) { moveFrom(std::move(other)); return *this; }
+    Ten& 
+    operator=(Ten && other) { moveFrom(std::move(other)); return *this; }
 
+    template<typename R>
     explicit
-    Tensor(const TensorRef<const value_type,range_type>& ref) { assignFromRef(ref); }
+    Ten(TenRef<const value_type,R> const& ref) { assignFromRef(ref); }
 
+    template<typename R>
     explicit
-    Tensor(const TensorRef<value_type,range_type>& ref) { assignFromRef(ref); }
+    Ten(const TenRef<value_type,R>& ref) { assignFromRef(ref); }
 
-    Tensor&
-    operator=(const TensorRef<const value_type>& ref) { assignFromRef(ref); return *this; }
+    template<typename R>
+    Ten&
+    operator=(TenRef<const value_type,R> const& ref) { assignFromRef(ref); return *this; }
 
-    Tensor&
-    operator=(const TensorRef<value_type>& ref) { assignFromRef(ref); return *this; }
+    template<typename R>
+    Ten&
+    operator=(TenRef<value_type,R> const& ref) { assignFromRef(ref); return *this; }
 
     explicit operator bool() const { return !data_.empty(); }
 
@@ -206,16 +333,46 @@ class Tensor
     size_type
     stride(size_type i) const { return range_.stride(i); }
 
-    const range_type&
+    range_type const&
     range() const { return range_; }
 
-    template <typename... Inds>
-    reference
-    operator()(Inds&&... ii) { return data_[ind(range_,std::forward<Inds>(ii)...)]; }
+    const_reference
+    operator()() const 
+        { 
+#ifdef DEBUG
+        if(r() != 0) throw std::runtime_error("No indices passed to rank > 0 Ten");
+#endif
+        return *data_; 
+        }
 
     template <typename... Inds>
     const_reference
-    operator()(Inds&&... ii) const { return data_[ind(range_,std::forward<Inds>(ii)...)]; }
+    operator()(Inds&&... ii) const 
+        { 
+#ifdef DEBUG
+        if(sizeof...(ii) != r()) throw std::runtime_error("Wrong number of indices passed to Ten");
+#endif
+        return data_[ind(range_,std::forward<Inds>(ii)...)]; 
+        }
+
+    reference
+    operator()()
+        { 
+#ifdef DEBUG
+        if(r() != 0) throw std::runtime_error("No indices passed to rank > 0 Ten");
+#endif
+        return *data_; 
+        }
+
+    template <typename... Inds>
+    reference
+    operator()(Inds&&... ii)
+        { 
+#ifdef DEBUG
+        if(sizeof...(ii) != r()) throw std::runtime_error("Wrong number of indices passed to Ten");
+#endif
+        return data_[ind(range_,std::forward<Inds>(ii)...)]; 
+        }
 
     iterator
     begin() { return data_.begin(); }
@@ -244,10 +401,10 @@ class Tensor
     void
     clear() { data_.clear(); range_.clear(); }
 
-    storage_type&
+    storage_type &
     store() { return data_; }
 
-    const storage_type&
+    storage_type const&
     store() const { return data_; }
 
     private:
@@ -262,15 +419,16 @@ class Tensor
         data_.assign(len,0.);
         }
 
+    template<typename R>
     void
-    assignFromRef(const TensorRef<const value_type>& other)
+    assignFromRef(TenRef<const value_type,R> const& other)
         {
         //Copy data from other contiguously into data_
         data_ = storage_type(other.cbegin(),other.cend());
         }
 
     void
-    assignFrom(const Tensor& other)
+    assignFrom(Ten const& other)
         {
         if(&other == this) return;
         range_ = other.range_;
@@ -278,7 +436,7 @@ class Tensor
         }
 
     void
-    moveFrom(Tensor&& other)
+    moveFrom(Ten && other)
         {
         range_ = std::move(other.range_);
         data_ = std::move(other.data_);
@@ -287,11 +445,12 @@ class Tensor
     };
 
 template<typename T, typename R>
-void TensorRef<T,R>::
+void TenRef<T,R>::
 pointTo(tensor_type& t)
     {
     pdata_ = t.data();
     prange_ = &t.range();
+    range_.clear();
     }
 
 
@@ -301,48 +460,58 @@ pointTo(tensor_type& t)
 
 template<typename T, typename R>
 auto
-makeRef(TensorRef<T,R>& t) { return t; }
+makeRef(TenRef<T,R> & t) { return t; }
 
 template<typename T, typename R>
 auto
-makeRef(TensorRef<const T,R>& t) { return t; }
+makeRef(TenRef<const T,R> & t) { return t; }
 
 template<typename T, typename R>
 auto
-makeRef(Tensor<T,R>& t) { return TensorRef<T,R>(t); }
+makeRef(Ten<T,R> & t) { return TenRef<T,R>(t); }
 
 template<typename T, typename R>
 auto
-makeRef(const Tensor<T,R>& t) { return TensorRef<const T>(t); }
+makeRef(Ten<T,R> const& t) { return TenRef<const T,R>(t); }
 
 //This version of makeRef intended to fail,
-//forbids explicitly making TensorRefs to temporaries
+//forbids explicitly making TenRefs to temporaries
 template<typename T, typename R, typename... Rest>
 auto
-makeRef(Tensor<T,R>&& t, Rest&&... args) { return TensorRef<T,R>(std::move(t)); }
+makeRef(Ten<T,R> && t, Rest&&... args) { return TenRef<T,R>(std::move(t)); }
 
 template<typename T, typename R>
 auto
-makeRefc(TensorRef<T,R>& t) { return TensorRef<const T,R>(t); }
+makeRefc(TenRef<T,R> & t) { return TenRef<const T,R>(t); }
 
 template<typename T, typename R>
 auto
-makeRefc(TensorRef<const T,R>& t) { return t; }
+makeRefc(TenRef<const T,R> & t) { return t; }
 
 template<typename T, typename R>
 auto
-makeRefc(Tensor<T,R>& t) { return TensorRef<const T,R>(t); }
+makeRefc(Ten<T,R> & t) { return TenRef<const T,R>(t); }
 
 template<typename T, typename R>
 auto
-makeRefc(const Tensor<T,R>& t) { return TensorRef<const T>(t); }
+makeRefc(Ten<T,R> const& t) { return TenRef<const T,R>(t); }
 
 //This version of makeRefc intended to fail,
-//forbids explicitly making TensorRefs to temporaries
+//forbids explicitly making TenRefs to temporaries
 template<typename T, typename R, typename... Rest>
 auto
-makeRefc(Tensor<T,R>&& t, Rest&&... args) { return TensorRef<const T,R>(std::move(t)); }
+makeRefc(Ten<T,R> && t, Rest&&... args) { return TenRef<const T,R>(std::move(t)); }
+
+template<typename V, typename R>
+std::ostream&
+operator<<(std::ostream & s, TenRef<V,R> const& T);
+
+template<typename V, typename R>
+std::ostream&
+operator<<(std::ostream & s, Ten<V,R> const& T);
 
 } //namespace itensor
+
+#include "ten.ih"
 
 #endif
