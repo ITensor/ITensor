ae08e16fb7994823c86d47cccef5cc92083e181c
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2015-08-11 19:34:15 -0400

    Created tensor/slicerange.h
    Slicing range objects can be used as a step for slicing tensor objects.

---
+++ itensor/tensor/slicerange.h
@@ -0,0 +1,44 @@
+//
+// Distributed under the ITensor Library License, Version 1.2.
+//    (See accompanying LICENSE file.)
+//
+#ifndef __ITENSOR_SLICERANGE_H_
+#define __ITENSOR_SLICERANGE_H_
+
+#include "itensor/tensor/range.h"
+
+namespace itensor {
+
+template<typename Range_, typename Perm_>
+auto
+permuteRange(Range_ && R,
+             Perm_ const& P)
+    {
+    auto Rb = RangeBuilder(R.r());
+    size_t n = 0;
+    for(auto pn : P)
+        {
+        Rb.setExtStr(pn,R.extent(n),R.stride(n));
+        ++n;
+        }
+    return Rb.build();
+    }
+
+template<typename Range_, typename Perm_>
+auto
+permuteExtents(Range_ && R,
+               Perm_ const& P)
+    {
+    auto Rb = RangeBuilder(R.r());
+    size_t n = 0;
+    for(auto pn : P)
+        {
+        Rb.setExtent(pn,R.extent(n));
+        ++n;
+        }
+    return Rb.build();
+    }
+
+} //namespace itensor
+
+#endif
