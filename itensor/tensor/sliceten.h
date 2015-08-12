3c38c6455185c132621402b9359e57db94a4dac0
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2015-08-12 18:11:43 -0400

    Added subIndex and groupInds tensor slicing methods, for use in condensing IQTensor

--- itensor/tensor/sliceten.h
+++ itensor/tensor/sliceten.h
@@ -63,9 +63,38 @@ subTensor(Ten_ && T,
     return makeTenRef(T.data()+offset,rb.build());
     }
 
+template<typename Ten_>
+auto
+subIndex(Ten_ && T,
+         size_t ind,
+         size_t start,
+         size_t stop)
+    {
+#ifdef DEBUG
+    if(ind >= size_t(T.r())) throw std::runtime_error("subIndex: index out of range");
+#endif
+    auto R = T.range();
+    R[ind].ext = stop-start;
+    return makeTenRef(T.data()+T.stride(ind)*start,std::move(R));
+    }
 
-/////////////////////////////
-/////////////////////////////
+template<typename Ten_>
+auto
+groupInds(Ten_ && T,
+          size_t istart,
+          size_t iend)
+    {
+    if(not isContiguous(T.range())) Error("groupInds requires contiguous range");
+    auto ngroup = iend-istart;
+    size_t nr = T.r()-ngroup+1;
+    auto rb = RangeBuilder(nr);
+    for(size_t j = 0; j < istart; ++j) rb.nextExtent(T.extent(j));
+    auto group_ext = 1;
+    for(size_t j = istart; j < iend; ++j) group_ext *= T.extent(j);
+    rb.nextExtent(group_ext);
+    for(size_t j = iend; j < size_t(T.r()); ++j) rb.nextExtent(T.extent(j));
+    return makeTenRef(T.data(),rb.build());
+    }
 
 template<typename T, typename R, typename Perm_>
 auto
