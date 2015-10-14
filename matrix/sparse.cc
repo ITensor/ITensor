f9a23cf94c14de1f7ea6d5136aed02da4759a355
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2015-10-14 14:59:02 -0400

    Merged matrix/hpsortir.cc into matrix/sparse.cc

--- matrix/sparse.cc
+++ matrix/sparse.cc
@@ -17,6 +17,54 @@ using std::istream;
 using std::ostream;
 using std::endl;
 
+void 
+hpsortir(int n,int* ra,Real* rb)
+    {
+    int l,j,ir,i;
+    int rra;
+    Real rrb;
+
+    ra--; rb--;			// Input arrays start at 0, Num. Rec. at 1
+
+    if (n < 2) return;
+    l=(n >> 1)+1;
+    ir=n;
+    for (;;) 
+    {
+        if (l > 1) 
+            {
+            rra=ra[--l];
+            rrb=rb[l];
+            } 
+        else 
+            {
+            rra=ra[ir];
+            rrb=rb[ir];
+            ra[ir]=ra[1];
+            rb[ir]=rb[1];
+            if(--ir == 1) 
+                {
+                ra[1]=rra;
+                rb[1]=rrb;
+                return;
+                }
+            }
+        i=l;
+        j=l << 1;
+        while (j <= ir)	{
+            if (j < ir && ra[j] < ra[j+1]) ++j;
+            if (rra < ra[j]) {
+            ra[i]=ra[j];
+            rb[i]=rb[j];
+            j += (i=j);
+            }
+            else j=ir+1;
+        }
+        ra[i]=rra;
+        rb[i]=rrb;
+    } //end for
+    }
+
 void SparseVector::AddElement(int i, Real value) 
 // SparseMatrix must make sure the storage is big enough.
     {
