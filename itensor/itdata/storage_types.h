4190f043216e5a6f8da3d42b5120cc098bb6b12d
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2015-11-02 11:58:04 -0500

    Renamed itdiag.h/cc to diag.h/cc

--- itensor/itdata/storage_types.h
+++ itensor/itdata/storage_types.h
@@ -59,7 +59,7 @@ Diag<Cplx>
 //(3) Register header file names
 #ifdef REGISTER_ITDATA_HEADER_FILES
 #include "itensor/itdata/dense.h"
-#include "itensor/itdata/itdiag.h"
+#include "itensor/itdata/diag.h"
 #include "itensor/itdata/itcombiner.h"
 //#include "itensor/itdata/iqtreal.h"
 //#include "itensor/itdata/iqtcombiner.h"
