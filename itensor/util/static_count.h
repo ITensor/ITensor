fcb3a128fdbe4e9be1be9df58c8eac388bc54b67
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2015-11-18 11:57:25 -0500

    Added StaticCount profiling utility

---
+++ itensor/util/static_count.h
@@ -0,0 +1,33 @@
+#ifndef __ITENSOR_STATIC_COUNT_H__
+#define __ITENSOR_STATIC_COUNT_H__
+
+namespace itensor {
+
+//
+// Usage:
+//
+//    static auto sc = StaticCount("Incremented %d times");
+//    ++sc;
+//    //will automatically print "Incremented ## times"
+//    //when program ends
+//
+
+struct StaticCount
+    {
+    long count = 0;
+    const char* fstring = "";
+
+    StaticCount(const char* fstring_) : fstring(fstring_) { }
+
+    ~StaticCount()
+        {
+        printfln(fstring,count);
+        }
+
+    void
+    operator++() { ++count; }
+    };
+
+} //namespace itensor
+
+#endif
