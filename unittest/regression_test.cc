ba74e82f2269edd0beb1ebf9e10d7c6887b0e7c0
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2016-03-15 15:59:19 -0700

    Updated regression_test to compile with latest code

--- unittest/regression_test.cc
+++ unittest/regression_test.cc
@@ -20,15 +20,15 @@ TEST_CASE("ITensor Times IQIndexVal")
 
     Index l("l",4);
     ITensor T(l);
-    T.randomize();
+    randomize(T);
 
     ITensor R = T * s(2);
 
     REQUIRE(hasindex(R,s));
-    CHECK(R(l(1),s(1)) == 0);
-    CHECK(R(l(2),s(1)) == 0);
-    CHECK(R(l(3),s(1)) == 0);
-    CHECK(R(l(4),s(1)) == 0);
+    CHECK(R.real(l(1),s(1)) == 0);
+    CHECK(R.real(l(2),s(1)) == 0);
+    CHECK(R.real(l(3),s(1)) == 0);
+    CHECK(R.real(l(4),s(1)) == 0);
     }
 
 TEST_CASE("ITensor from IQIndexVal")
@@ -36,13 +36,13 @@ TEST_CASE("ITensor from IQIndexVal")
     IQIndex s("S",Index("up",1,Site),QN(+1),
                   Index("dn",1,Site),QN(-1));
 
-    ITensor T1(s(1));
-    CHECK(T1(s(1)) == 1);
-    CHECK(T1(s(2)) == 0);
+    auto T1 = setElt(s(1));
+    CHECK(T1.real(s(1)) == 1);
+    CHECK(T1.real(s(2)) == 0);
 
-    ITensor T2(s(2));
-    CHECK(T2(s(1)) == 0);
-    CHECK(T2(s(2)) == 1);
+    auto T2 = setElt(s(2));
+    CHECK(T2.real(s(1)) == 0);
+    CHECK(T2.real(s(2)) == 1);
     }
 
 TEST_CASE("CombinerOrder")
@@ -50,23 +50,23 @@ TEST_CASE("CombinerOrder")
     Index a("a",2),c("c",2);
 
     ITensor U(a,c);
-    U(a(1),c(2)) = 1;
+    U.set(a(1),c(2),1);
     //PrintDat(U);
 
-    Combiner C(c,a);
-    ITensor CU = C * U;
+    auto C = combiner(c,a);
+    auto CU = C * U;
     //PrintDat(CU);
-    ITensor UU = C * CU;
+    auto UU = C * CU;
     //PrintDat(UU);
 
-    CHECK((U-UU).norm() < 1E-10);
+    CHECK(norm(U-UU) < 1E-10);
 
-    Combiner D(a,c);
+    auto D = combiner(a,c);
     CU = D * U;
     //PrintDat(CU);
     UU = D * CU;
 
-    CHECK((U-UU).norm() < 1E-10);
+    CHECK(norm(U-UU) < 1E-10);
     }
 
 TEST_CASE("SVDIndexOrder")
@@ -74,8 +74,8 @@ TEST_CASE("SVDIndexOrder")
     Index a("a",2),b("b",1),c("c",2);
 
     ITensor z(c,a,b);
-    z(c(1),a(2),b(1)) = 1;
-    z(c(1),a(1),b(1)) = 1;
+    z.set(c(1),a(2),b(1),1);
+    z.set(c(1),a(1),b(1),1);
 
 
     ITensor V(b);
@@ -98,7 +98,7 @@ TEST_CASE("SVDIndexOrder")
     //PrintDat(U);
 
     ITensor nz = U*D*V;
-    CHECK((z-nz).norm() < 1E-10);
+    CHECK(norm(z-nz) < 1E-10);
 
     }
 
@@ -110,7 +110,7 @@ TEST_CASE("SVDArrows")
     IQTensor AA(L,R);
 
     ITensor block(l,r);
-    block.randomize();
+    randomize(block);
     AA += block;
 
     const QN Zero;
@@ -124,35 +124,35 @@ TEST_CASE("SVDArrows")
     CHECK_EQUAL(div(V),Zero);
     }
 
-TEST_CASE("ExpandIndex")
-    {
-    //
-    //ITensor::expandIndex was
-    //failing when the first Index 
-    //had m == 1 and the second or
-    //third Index was to be expanded.
-    //The reason is that expandIndex
-    //assumed a stable Index order
-    //whereas the ITensor constructor
-    //moves m==1's to the back
-    //
-    Index l("l");
-
-    Index emp("emp"),occ("occ");
-    IQIndex S("S",emp,QN(0,0),
-                  occ,QN(1,0),
-                  Out);
-
-    ITensor oo(l,occ,prime(occ));
-    oo(l(1),occ(1),prime(occ)(1)) = 1;
-
-    oo.expandIndex(occ,S,offset(S,occ));
-
-    oo.expandIndex(prime(occ),prime(S),offset(S,occ));
-
-    CHECK_CLOSE(0,oo(S(1),prime(S)(1),l(1)),1E-5);
-    CHECK_CLOSE(1,oo(S(2),prime(S)(2),l(1)),1E-5);
-    }
+//TEST_CASE("ExpandIndex")
+//    {
+//    //
+//    //ITensor::expandIndex was
+//    //failing when the first Index 
+//    //had m == 1 and the second or
+//    //third Index was to be expanded.
+//    //The reason is that expandIndex
+//    //assumed a stable Index order
+//    //whereas the ITensor constructor
+//    //moves m==1's to the back
+//    //
+//    Index l("l");
+//
+//    Index emp("emp"),occ("occ");
+//    IQIndex S("S",emp,QN(0,0),
+//                  occ,QN(1,0),
+//                  Out);
+//
+//    ITensor oo(l,occ,prime(occ));
+//    oo(l(1),occ(1),prime(occ)(1)) = 1;
+//
+//    oo.expandIndex(occ,S,offset(S,occ));
+//
+//    oo.expandIndex(prime(occ),prime(S),offset(S,occ));
+//
+//    CHECK_CLOSE(0,oo(S(1),prime(S)(1),l(1)),1E-5);
+//    CHECK_CLOSE(1,oo(S(2),prime(S)(2),l(1)),1E-5);
+//    }
 
 TEST_CASE("ConvertToITensor")
     {
@@ -162,16 +162,16 @@ TEST_CASE("ConvertToITensor")
                   occ,QN(1,0),
                   Out);
 
-    IQTensor T(L,S,prime(S));
-    T(L(1),S(1),prime(S)(1)) = 1;
-    T(L(1),S(2),prime(S)(2)) = 1;
+    auto T = IQTensor(L,dag(S),prime(S));
+    T.set(L(1),S(1),prime(S)(1),1);
+    T.set(L(1),S(2),prime(S)(2),1);
 
-    ITensor t = T.toITensor();
+    auto t = toITensor(T);
 
-    CHECK_CLOSE(1,t(L(1),S(1),prime(S)(1)),1E-5);
-    CHECK_CLOSE(1,t(L(1),S(2),prime(S)(2)),1E-5);
-    CHECK_CLOSE(0,t(L(1),S(2),prime(S)(1)),1E-5);
-    CHECK_CLOSE(0,t(L(1),S(2),prime(S)(1)),1E-5);
+    CHECK_CLOSE(1,t.real(L(1),S(1),prime(S)(1)));
+    CHECK_CLOSE(1,t.real(L(1),S(2),prime(S)(2)));
+    CHECK_CLOSE(0,t.real(L(1),S(2),prime(S)(1)));
+    CHECK_CLOSE(0,t.real(L(1),S(2),prime(S)(1)));
     }
 
 
@@ -217,54 +217,6 @@ TEST_CASE("IndexOrder")
     }
     */
 
-TEST_CASE("IdentComplexMult")
-    {
-    //
-    // Aug 25, 2013: contracting product working incorrectly
-    // for 2x2 identity-type ITensor contracted with 2x2 complex
-    // ITensor
-    //
-    // Problem appears to be that the complex ITensor helper code at
-    // the top of ITensor::operator*= *assumes* same resulting index
-    // order for separate real- and imaginary-part contractions
-    //
-
-    Index d("d",2),
-          l("l",2),
-          c("c",2);
-
-    ITensor br(c,l),
-            bi(c,l);
-
-    commaInit(br,c,l) << 11,12,
-                         0 ,0 ;
-    commaInit(bi,c,l) << 11,12,
-                         0 ,0 ;
-
-    ITensor b = br+Complex_i*bi;
-
-
-    Matrix C(2,2);
-    C = 0;
-    C.Diagonal() = 1;
-
-    ITensor conv(d,c,C);
-
-    ITensor rr(d,l),
-            ri(d,l);
-
-    commaInit(rr,d,l) << 11,12,
-                         0 ,0 ;
-    commaInit(ri,d,l) << 11,12,
-                         0 ,0 ;
-
-    ITensor res = rr + Complex_i*ri;
-
-    ITensor Diff = conv*b-res;
-
-    CHECK(Diff.norm() < 1E-10);
-    }
-
 TEST_CASE("ComplexAddition")
     {
     //EMS Oct 21, 2013
@@ -275,20 +227,20 @@ TEST_CASE("ComplexAddition")
           S1("S1",2);
 
     ITensor a(L1,S1);
-    a(L1(1),S1(2)) = 21;
-    a(L1(2),S1(2)) = 22;
+    a.set(L1(1),S1(2),21);
+    a.set(L1(2),S1(2),22);
     a.scaleTo(-1.2243);
 
     ITensor ca = a*Complex_i;
 
     ITensor b(S1,L1);
-    b(S1(1),L1(3)) = 13;
-    b(S1(1),L1(4)) = 14;
+    b.set(S1(1),L1(3),13);
+    b.set(S1(1),L1(4),14);
     b.scaleTo(9.3435);
 
     ITensor r = b + ca;
 
-    CHECK((realPart(r)-b).norm() < 1E-12);
-    CHECK((imagPart(r)-a).norm() < 1E-12);
+    CHECK(norm(realPart(r)-b) < 1E-12);
+    CHECK(norm(imagPart(r)-a) < 1E-12);
     }
 
