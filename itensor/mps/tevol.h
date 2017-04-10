f5bcf819d6958890654fed13f757662cae583392
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2017-04-10 14:00:38 -0700

    Fixed bug in new, more efficient version of gateTEvol (did timings showing it is significantly faster)

--- itensor/mps/tevol.h
+++ itensor/mps/tevol.h
@@ -69,33 +69,37 @@ gateTEvol(Iterable const& gatelist,
     for(int tt = 1; tt <= nt; ++tt)
         {
         auto g = gatelist.begin();
-        auto i1 = g->i1();
-        auto i2 = g->i2();
-        auto AA = psi.A(i1)*psi.A(i2)*g->gate();
-        AA.mapprime(1,0,Site);
-        for(++g; g != gatelist.end(); ++g)
+        while(g != gatelist.end())
             {
-            //Last i1,i2
-            auto l1 = i1;
-            auto l2 = i2;
-            //Current i1,i2
-            i1 = g->i1();
-            i2 = g->i2();
-            //SVD AA to restore MPS form
-            //before applying current gate
-            if(i1 >= l2)
-                {
-                psi.svdBond(l1,AA,Fromleft,args);
-                psi.position(i1); //does no work if position already i1
-                }
-            else if(i2 <= l1)
-                {
-                psi.svdBond(l1,AA,Fromright,args);
-                psi.position(i2); //does no work if position already i2
-                }
-            //Apply current gate
-            AA = psi.A(i1)*psi.A(i2)*g->gate();
+            auto i1 = g->i1();
+            auto i2 = g->i2();
+            auto AA = psi.A(i1)*psi.A(i2)*g->gate();
             AA.mapprime(1,0,Site);
+
+            ++g;
+            if(g != gatelist.end())
+                {
+                //Look ahead to next gate position
+                auto ni1 = g->i1();
+                auto ni2 = g->i2();
+                //SVD AA to restore MPS form
+                //before applying current gate
+                if(ni1 >= i2)
+                    {
+                    psi.svdBond(i1,AA,Fromleft,args);
+                    psi.position(ni1); //does no work if position already ni1
+                    }
+                else
+                    {
+                    psi.svdBond(i1,AA,Fromright,args);
+                    psi.position(ni2); //does no work if position already ni2
+                    }
+                }
+            else
+                {
+                //No next gate to analyze, just restore MPS form
+                psi.svdBond(i1,AA,Fromright,args);
+                }
             }
 
         if(normalize)
