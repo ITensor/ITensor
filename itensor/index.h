6a5cc8252e32111b13999b4beb09e82dbb6e6ce4
ybarlev <yevgeny.barlev@gmail.com>
2017-05-25 20:15:40 -0400

    Adds a method to set the size of the index

--- itensor/index.h
+++ itensor/index.h
@@ -88,6 +88,9 @@ class Index
     // Returns the bond dimension
     long 
     m() const { return m_; }
+    // Sets the bond dimension, use with care
+    void
+    setm(int m) { m_ = m; }
 
     // Returns the prime level
     int 
