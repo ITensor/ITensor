d14b5c4a6de179a1bbdcc24b8dfc45eb354a0e1b
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2017-10-09 13:58:45 -0400

    Relaxed error criterion for floating point comparisons in test suite

--- unittest/test.h
+++ unittest/test.h
@@ -4,7 +4,7 @@
 #include "catch.hpp"
 
 #define CHECK_EQUAL(X,Y) CHECK((X) == (Y))
-#define CHECK_CLOSE(X,Y) CHECK(std::norm((X)-(Y)) < 1E-12)
+#define CHECK_CLOSE(X,Y) CHECK(std::norm((X)-(Y)) < 1E-10)
 //#define CHECK_CLOSE(X,Y)\
 //    if(std::norm(X) < std::norm(Y)) CHECK(std::norm((X)-(Y)) < 1E-10*std::norm(Y));\
 //    else CHECK(std::norm((X)-(Y)) < 1E-10*std::norm(X))
