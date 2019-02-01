5b5f11e31f8618d495cc584550e143ef68222902
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2019-02-01 15:04:33 -0800

    Added unit tests for Index tag functions

--- unittest/index_test.cc
+++ unittest/index_test.cc
@@ -70,4 +70,42 @@ TEST_CASE("IndexTest")
         CHECK(i2.tags() == i.tags());
         CHECK(i2.primeLevel() == 0);
         }
+
+    SECTION("Tag Basics")
+        {
+        auto i = Index(3);
+
+        auto ia = addTags(i,"a");
+        CHECK(hasTags(ia,"a"));
+        CHECK(ia != i);
+        CHECK(ia == ia);
+
+        auto ib = addTags(i,"b");
+        CHECK(hasTags(ib,"b"));
+        CHECK(ib != i);
+        CHECK(ib == ib);
+
+        CHECK(ia != ib);
+
+        auto iab1 = addTags(ia,"b");
+        auto iab2 = addTags(ib,"a");
+        CHECK(iab1 == iab2);
+        CHECK(hasTags(iab1,"a"));
+        CHECK(hasTags(iab1,"b"));
+        CHECK(hasTags(iab1,"a,b"));
+
+        CHECK(removeTags(iab1,"a,b") == i);
+        CHECK(removeTags(iab1,"b") == ia);
+        CHECK(removeTags(iab1,"a") == ib);
+
+        auto ic = setTags(iab1,"c");
+        CHECK(hasTags(ic,"c"));
+
+        CHECK(hasTags(replaceTags(ic,"c","a"),"a"));
+        CHECK(hasTags(replaceTags(ic,"c","a,b"),"a,b"));
+        CHECK(hasTags(replaceTags(ic,"","a,b"),"a,b"));
+        CHECK(!hasTags(replaceTags(ic,"c","a,b"),"c"));
+        CHECK(hasTags(addTags(ic,"a,b"),"a,b,c"));
+        }
+
     }
