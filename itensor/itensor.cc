a36e4e51c10dcd4079412b8d89bcb1f1497a1e6c
mtfishman <mtfishman@users.noreply.github.com>
2019-12-03 12:35:39 -0500

    Improve eltC() error message (again)

--- itensor/itensor.cc
+++ itensor/itensor.cc
@@ -84,8 +84,8 @@ eltC() const
     {
     if(inds().order() != 0)
         {
-        Error(format("Wrong number of IndexVals passed to elt/eltC (expected 0, got %d)",inds().order()));
         PrintData(inds());
+        Error(format("Wrong number of IndexVals passed to elt/eltC (expected 0, got %d)",inds().order()));
         }
     constexpr size_t size = 0;
     auto inds = IntArray(size);
